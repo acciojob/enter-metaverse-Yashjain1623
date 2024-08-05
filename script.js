6538a5cd53d4011dd60fdf19ad81784c02d9f18a
Yashjain1623 <157748380+Yashjain1623@users.noreply.github.com>
2024-08-05 12:16:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const statusElement = document.getElementById('status');
+const enterBtn = document.getElementById('enterBtn');
+function handleEnter() {
+    // Create a new <h1> element with the new text
+    const newElement = document.createElement('h1');
+    newElement.textContent = 'Entered Metaverse';
+
+
+	const parentElement = statusElement.parentElement;
+    parentElement.replaceChild(newElement, statusElement);
+}
+
+// Attach event listener to the button
+enterBtn.addEventListener('click', handleEnter);
